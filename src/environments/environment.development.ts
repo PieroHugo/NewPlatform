12e808d3684b52c005ab67260f359a8558e5fed8
Piero Elescano <pieroelescano300@gmail.com>
2025-09-25 23:10:38 -0500

    feat: update files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,7 @@ export const environment = {
   newsProviderApiBaseUrl: 'https://newsapi.org/v2',
   newsProviderNewsEndpointPath: '/top-headlines',
   newsProviderSourcesEndpointPath: '/sources',
-  newsProviderApiKey: '',
+  newsProviderApiKey: '53c412b6b6a44edbbb300a500c344508',
   logoProviderApiBaseUrl: 'https://img.logo.dev/',
-  logoProviderApiKey: ""
+  logoProviderApiKey: "sk_WvJaoDL-QJ6JVOkOhnq7Cg"
 };
